24d776d3fa09598eb60c5a8b7adff51c1c8a9087
Jason Tan <taz5176@hotmail.com>
2023-03-19 23:52:20 +0800

    implement summary table

--- class_example.py
+++ class_example.py
@@ -6,36 +6,36 @@ class Device():
     Device class to store each test result
     """
 
-    def __init__(self, dt, loop, result):
+    def __init__(self, dt, loop, test_result):
         """
         Device class constructor
 
         Args:
             dt (str): current datetime string
             loop (int): current test loop count
-            result (str): test result
+            test (str): test result
         """
         self.result = {
             'dt': dt,
             'loop': loop,
             'status': None,
             'retry': 0,
-            'result': result
+            'test': test_result
         }
         
         
-    def update(self, status, retry, result):
+    def update(self, status, retry, test):
         """
         Method to update class attributes
 
         Args:
             status (str): current device status
-            retry (_type_): current retry count
-            result (_type_): test result
+            retry (int): current retry count
+            test (str): test result
         """
         self.update_device_status(status)
         self.update_device_retry(retry)
-        self.update_device_result(result)
+        self.update_device_result(test)
         
 
     def update_device_dt(self, dt):
@@ -78,14 +78,14 @@ class Device():
         self.result['retry'] = retry
         
         
-    def update_device_result(self, result):
+    def update_device_result(self, test):
         """
         Method to update class result attribute
 
         Args:
             result (str): current test result
         """
-        self.result['result'] = result
+        self.result['test'] = test
 
 
 class Testing(Device):
@@ -108,13 +108,19 @@ class Testing(Device):
         """
         self.test_retries = retries
         self.all_results = []
+        self.summary_result = {
+            'Normal': 0,
+            'Abnormal': 0,
+            'No Device': 0
+        }
+        self.summary_table = []
 
 
     # Constants for status
     STATUS = {
         'NORMAL': 'Normal',
         'ABNORMAL': 'Abnormal',
-        'NO_DISK': 'No Disk'
+        'NO_DEVICE': 'No Device'
     }
     
     # Constants for results
@@ -125,7 +131,7 @@ class Testing(Device):
     }
         
         
-    def update_test_result(self, device):
+    def update_result(self, device):
         """
         Method to updata all_results attribute
 
@@ -141,7 +147,55 @@ class Testing(Device):
         """
         for i in self.all_results:
             print(i.result)
-            
+    
+
+    def get_status_summary(self):
+        """
+        Method to calculate test summary
+        """
+        # prepare summary result
+        for i in self.all_results:
+            if i.result['retry'] == 0:
+                self.summary_result[i.result['status']] += 1
+
+    
+    def print_status_summary(self):
+        """
+        Method to print test summary
+        """
+        self.get_status_summary()
+        print('\nStatus counts:')
+        for k, v in self.summary_result.items():
+            print(f'{k}: {v}')
+
+
+    def get_summary_table(self):
+        for n, i in enumerate(self.all_results):
+            if i.result['retry'] == 0:
+                each_loop = {'Loop':i.result['loop']}
+                each_loop.update({'Status':i.result['status']})
+                self.summary_table.append(each_loop)
+                # print(i.result['test'])
+
+            result = f'{"Retry" if i.result["retry"] > 0 else ""} {i.result["test"]}'
+            self.summary_table[i.result['loop']].update({'Result':result})
+
+
+    def print_summary_table(self):
+        self.get_summary_table()
+        
+        print('\nSummary table:')
+        print(' Loop    Status     Result')
+        for i in self.summary_table:
+            for k, v in i.items():
+                if k == 'Loop':
+                    print(f' {v:>4}', end='')
+                elif k == 'Status':
+                    print(f' {v:>9}', end='')
+                elif k == 'Result':
+                    print(f' {v:>10}', end='')
+            print()
+
     
     def run(self, loops=10):
         """
@@ -157,7 +211,7 @@ class Testing(Device):
 
             for retry in range(self.test_retries+1):
                 device = Device(Testing.get_dt(), 
-                                loop+1,
+                                loop,
                                 Testing.RESULT['NOT_RUN'])
                 
                 # simulate run test to get status
@@ -167,21 +221,21 @@ class Testing(Device):
                 if status == Testing.STATUS['NORMAL']:
                     result = Testing.RESULT['PASS']
                     device.update(status, retry, result)
-                    self.update_test_result(device)
+                    self.update_result(device)
                     break
                 
                 elif status == Testing.STATUS['ABNORMAL']:
                     result = Testing.RESULT['FAIL']
                     device.update(status, retry, result)
-                    self.update_test_result(device)
+                    self.update_result(device)
                     break
                 
-                elif status == Testing.STATUS['NO_DISK']:
+                elif status == Testing.STATUS['NO_DEVICE']:
                     result = Testing.RESULT['FAIL']
                     device.update(status, retry, result)
-                    self.update_test_result(device)
+                    self.update_result(device)
                     if retry == 0:
-                        print('>>>> No Disk Found, Beginning Retry')
+                        print('>>>> No Device Found, Beginning Retry')
                     if retry < self.test_retries:
                         print(f'>>>> Retry Count {retry+1}:', end=' --- ')
                 
@@ -205,11 +259,10 @@ def get_device_status():
     
     # Simulate device status
     status = random.choice(["Normal", 
-                            "Abnormal", 
-                            "No Disk", 
-                            "No Disk", 
-                            "No Disk"])
-    time.sleep(1)  # Simulate processing time
+                            "No Device", 
+                            "No Device", 
+                            "No Device"])
+    # time.sleep(1)  # Simulate processing time
     return status
 
 
@@ -219,9 +272,10 @@ def main():
     """
     test = Testing()
     test.run(loops=10)
-
     # test.print_all_results()
-    
+    test.print_status_summary()
+    test.print_summary_table()
 
+    
 if __name__ == '__main__':
     main()
